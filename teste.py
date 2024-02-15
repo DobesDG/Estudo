19d09730a57313d4f728f2ca633517447754a243
Douglas Galvão <douglasgalvao06@gmail.com>
2024-02-15 19:41:10 -0300

    Primeiro Estudo de Python

---
+++ teste.py
@@ -0,0 +1,12 @@
+name = "douglas"
+age = str(20)
+is_male = True
+
+if is_male == True:
+ gender = " e sou um homem."
+else:
+ gender = " e sou uma mulher"
+
+frase = "meu nome é " + name + " e tenho " + age + " anos," + gender
+
+print(frase.upper())
