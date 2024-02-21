104ee2b6a91b200871b97252e604659ad5b51334
Douglas Galvão <douglasgalvao06@gmail.com>
2024-02-21 20:12:17 -0300

    Cria interface de grid com botões das células e gera bombas

--- campominado.py
+++ campominado.py
@@ -1,6 +1,8 @@
 import random
+from random import sample
+import tkinter as tk
 
-x = random.randrange(0,225)
+
 grid = []
 
 #Choose difficulty
@@ -14,18 +16,45 @@ difficulty_choosen = int(input("Choose difficulty: "))
 def range_grid(difficulty_choosen):    
     if difficulty_choosen == 1:
         grid_num = 100
+        bomb_num = 10
     elif difficulty_choosen == 2:
-        grid_num = 625
+        grid_num = 225
+        bomb_num = 22
     elif difficulty_choosen == 3:
-        grid_num = 2500
-    return grid_num
+        grid_num = 400
+        bomb_num = 40
+    return (grid_num, bomb_num)
 
-grid_num = range_grid(difficulty_choosen)
+[grid_num, bomb_num] = range_grid(difficulty_choosen)
 
-
+#Define grid and set random bombs
 for x in range(0,grid_num):
     grid.append(x)
 
-print(grid)
+random_bombs = sample(grid , k = bomb_num)
+
+
+def criar_botao(idx):
+    botao = tk.Button(root, text=str(idx), width=5, height=2, command=lambda i=idx: botao_clicado(i))
+    botao.grid(row=idx // num_colls, column= idx % num_colls)
+
+def botao_clicado(idx):
+    print(f"Botão clicado: Index {idx}")
+
+
+root = tk.Tk()
+root.title("Minesweeper")
+
+num_rows = int(grid_num ** (1/2))
+num_colls = int(grid_num ** (1/2))
+all_btn = num_rows * num_colls
+
+for idx in range(all_btn):
+    criar_botao(idx)
+
+root.mainloop()
+
+
+
 
 
