07b84a1ad7b933586f28c04af69dbdb03f08ba61
Douglas Galvao <douglasgalvao06@gmail.com>
2024-04-05 15:42:01 -0300

    Delete Node

--- BT_Study.py
+++ BT_Study.py
@@ -12,7 +12,7 @@ class Node:
         else:
             if data < self.data:
                 if self.left is None:
-                        self.left = Node(data)
+                    self.left = Node(data)
                 else:
                     self.left.insert(data)
             elif data > self.data:
@@ -21,6 +21,40 @@ class Node:
                 else:
                     self.right.insert(data)
 
+def delete(r,node):
+    def minvalue(node):
+        while current and current.left is not None:
+            current = current.left
+            return current
+        
+    if r is None:
+        return
+    else:
+        if node < r.data:
+            if r.left is None:
+                print("This number is not in the Tree")
+            else:
+                r.left = delete(r.left, node)
+        elif node > r.data:
+            if r.right is None:
+                print("This number is not in the Tree")
+            else:
+                r.right = delete(r.right, node)
+        else: 
+            if r.left is None:
+                temp = r.right
+                r = None
+                return temp
+            elif r.right is None:
+                temp = r.left
+                r = None
+                return temp
+            temp = minvalue(r.right)
+
+            r.data = temp.data
+            r.right = delete(r.right, temp.data)
+    return r
+                
 def inorder(r):
     if r is None:
         return
@@ -81,7 +115,21 @@ def pos(r):
         if node.left:
             stack.append(node.left)
     print(f"Pre Order Loop Search:\n{visited}")
-    
+
+def nodesearh(r, node):
+    current = r
+    while current.data != node:
+        if current.right and current.left and node is not None:
+            if node < current.data:
+                 current = current.left
+            if node > current.data:
+                current = current.right
+            if current.data == node:
+                print("It's in the Tree")     
+        else:        
+            print("It's not in the Tree")
+            return       
+
 def order_choosen(order_print):
     global dict 
     dict = {}
@@ -99,6 +147,24 @@ def order_choosen(order_print):
         bfs(adjacency_list)
     elif order_print == 5:
         pos(root)
+    elif order_print == 6:
+        try:
+            node = int(input("Insert Number: "))
+        except ValueError:
+            print("Insert a root or a number")
+            order_choosen(6)
+            return
+        nodesearh(root,node)
+    elif order_print == 7:
+        try:
+            node = int(input("Insert Number: "))
+        except ValueError:
+            print("Insert a root or a number")
+            order_choosen(7)
+            return
+        delete(root,node)
+        inorder(root)
+
     else:
         print("Invalid Number")
 
@@ -112,7 +178,7 @@ if __name__ == '__main__':
     root.insert(55)
 
 def main():
-    orders = ["In Order","Pre Order","Post Order","Breath First","Pre Order Loop"]
+    orders = ["In Order","Pre Order","Post Order","Breath First","Pre Order Loop","Node Search", "Delete Node"]
     for idx, order in enumerate(orders):
         print("{}) {}".format(idx + 1, order))
     order_print = int(input("Choose a search order above: "))
